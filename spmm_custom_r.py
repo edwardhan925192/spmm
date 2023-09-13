2498c21d8b06ba6777f019e62e2b4132db7a30ff
edwardhan925151 <127165920+edwardhan925192@users.noreply.github.com>
2023-09-13 10:58:31 +0900

    custom r

--- spmm_custom_r.py
+++ spmm_custom_r.py
@@ -29,11 +29,7 @@ class SPMM_regressor(nn.Module):
         self.text_encoder = BertForMaskedLM(config=bert_config)
         for i in range(bert_config.fusion_layer, bert_config.num_hidden_layers):  self.text_encoder.bert.encoder.layer[i] = nn.Identity()
         self.text_encoder.cls = nn.Identity()
-        text_width = self.text_encoder.config.hidden_size
-
-        # Freeze BERT layers
-        for param in self.text_encoder.parameters():
-            param.requires_grad = True
+        text_width = self.text_encoder.config.hidden_size        
 
         # Projecting features denoising with additional dropout for regularization purpose
         self.feature_proj = nn.Sequential(
@@ -42,33 +38,19 @@ class SPMM_regressor(nn.Module):
             nn.Dropout(config['dropout_prob']),  
         )
 
-        self.reg_head = nn.Sequential(
+        self.reg_head1 = nn.Sequential(
             nn.Linear(text_width + config['feature_proj_dim'] , (text_width * 2) + config['feature_proj_dim'] ),
             nn.GELU(),
             nn.Linear((text_width * 2) + config['feature_proj_dim'], 1)
         )
 
-        # Unfreeze feature_proj and reg_head
-        for param in self.feature_proj.parameters():
-            param.requires_grad = True
-        for param in self.reg_head.parameters():
-            param.requires_grad = True
+        self.reg_diff = nn.Sequential(
+            nn.Linear(text_width + config['feature_proj_dim'] , (text_width * 2) + config['feature_proj_dim'] ),
+            nn.GELU(),
+            nn.Linear((text_width * 2) + config['feature_proj_dim'], 1)
+        )        
 
-        # ===================== param optim ===================== #
-    def get_optimizer_params(self, base_lr,config):
-    
-      # Separate parameters of BERT and regression head
-      bert_params = list(self.text_encoder.parameters())
-      reg_head_params = list(self.feature_proj.parameters()) + list(self.reg_head.parameters())
-      
-      # Assign learning rates
-      optimizer_grouped_parameters = [
-          {"params": bert_params, "lr": base_lr * config['bert_lprob']},  # Use 0.1x the base learning rate for BERT
-          {"params": reg_head_params, "lr": base_lr},    # Use the base learning rate for regression head
-      ]
-      return optimizer_grouped_parameters
-
-    def forward(self, text_input_ids, text_attention_mask,features, value, eval=False):
+    def forward(self, text_input_ids, text_attention_mask, features, value1, value2, difference, eval=False):
         vl_embeddings = self.text_encoder.bert(text_input_ids, attention_mask=text_attention_mask, return_dict=True, mode='text').last_hidden_state
         vl_embeddings = vl_embeddings[:, 0, :]
         
@@ -78,13 +60,41 @@ class SPMM_regressor(nn.Module):
         # Concatenate with vl_embeddings
         combined_representation = torch.cat([vl_embeddings, projected_features], dim=-1)
         
-        # Regression head
-        pred = self.reg_head(combined_representation).squeeze(-1)
+        # Predict with reg_head1
+        pred1 = self.reg_head1(combined_representation).squeeze(-1)
+        
+        # Predict the difference with reg_diff
+        pred_diff = self.reg_diff(combined_representation).squeeze(-1)
+        
+        # Combine pred1 and pred_diff to predict reg_head2
+        pred2 = pred1 + pred_diff
+        
+        if eval:
+            return pred1, pred2, pred_diff
 
-        if eval:    return pred
-        lossfn = nn.MSELoss()
-        loss = lossfn(pred, value)
-        return loss
+        lossfn = nn.MSELoss()        
+        
+        # loss1 = lossfn(pred1, value1)
+        # loss_diff = lossfn(pred_diff, difference)
+        loss2 = lossfn(pred2, value2)  
+        # combined_loss = loss1 + loss_diff + loss2
+        
+        return loss2
+
+
+    # ===================== param optim ===================== #
+    def get_optimizer_params(self, base_lr,config):
+    
+      # Separate parameters of BERT and regression head
+      bert_params = list(self.text_encoder.parameters())
+      reg_head_params = list(self.feature_proj.parameters()) + list(self.reg_head1.parameters()) + list(self.reg_diff.parameters())
+      
+      # Assign learning rates
+      optimizer_grouped_parameters = [
+          {"params": bert_params, "lr": base_lr * config['bert_lprob']},  # Use 0.1x the base learning rate for BERT
+          {"params": reg_head_params, "lr": base_lr},    # Use the base learning rate for regression head
+      ]
+      return optimizer_grouped_parameters
 
 def train(model, data_loader, feature_loader, optimizer, tokenizer, epoch,  device ):
     # train
@@ -98,15 +108,17 @@ def train(model, data_loader, feature_loader, optimizer, tokenizer, epoch,  devi
     combined_loader = zip(data_loader, feature_loader)
 
     tqdm_data_loader = tqdm(combined_loader, total=len(data_loader), miniters=print_freq, desc=header)
-    for i, ((text, value), features) in enumerate(tqdm_data_loader):
+    for i, ((text, value1, value2, difference), features) in enumerate(tqdm_data_loader):
         optimizer.zero_grad()
-        value = value.to(device, non_blocking=True)
+        value1 = value1.to(device, non_blocking=True)
+        value2 = value2.to(device, non_blocking=True)
+        difference = difference.to(device, non_blocking=True)
         features = features.to(device, non_blocking=True)  # Move features to the device
 
         text_input = tokenizer(text, padding='longest', truncation=True, max_length=100, return_tensors="pt").to(device)
 
         # Pass features to the model
-        loss = model(text_input.input_ids[:, 1:], text_input.attention_mask[:, 1:], features, value)
+        loss = model(text_input.input_ids[:, 1:], text_input.attention_mask[:, 1:], features, value1, value2, difference)
         loss.backward()
         optimizer.step()
 
@@ -114,45 +126,40 @@ def train(model, data_loader, feature_loader, optimizer, tokenizer, epoch,  devi
 
 @torch.no_grad()
 def evaluate(model, data_loader, feature_loader, tokenizer, device, denormalize=None, is_validation=True):
-    model.eval()
-    preds = []
-
-    # If it's validation, also collect ground truth values
-    if is_validation:
-        answers = []
+    model.eval()    
+    preds2 = []        
+    answers2 = []
+        
 
     # Use zip to combine data_loader and feature_loader
     combined_loader = zip(data_loader, feature_loader)
 
-    for (item, features) in combined_loader:
-        # Unpack depending on the data loader's output for validation or test
-        if is_validation:
-            text, value = item
-        else:
-            text = item[0]
-            value = None
+    for (item, features) in combined_loader:        
+        
+        text, value1, value2, difference = item        
 
         text_input = tokenizer(text, padding='longest', return_tensors="pt").to(device)
         features = features.to(device, non_blocking=True)  # Move features to the device
 
         # Pass features to the model
-        prediction = model(text_input.input_ids[:, 1:], text_input.attention_mask[:, 1:], features, value if value is not None else None, eval=True)
-        preds.append(prediction.cpu())
+        pred1, pred2, diff = model(text_input.input_ids[:, 1:], text_input.attention_mask[:, 1:], features, value1, value2, difference, eval=True)        
+        preds2.append(pred2.cpu())        
+        
+        value1 = value1.to(device, non_blocking=True)
+        value2 = value2.to(device, non_blocking=True)
+        difference = difference.to(device, non_blocking=True)            
+        answers2.append(value2.cpu())            
+    
+    preds2 = torch.cat(preds2, dim=0)            
+    answers2 = torch.cat(answers2, dim=0)        
+    
+    lossfn = nn.MSELoss()
+    
+    rmse2 = torch.sqrt(lossfn(preds2, answers2)).item()        
 
-        if is_validation:
-            value = value.to(device, non_blocking=True)
-            answers.append(value.cpu())
+    return rmse2
+    
 
-    preds = torch.cat(preds, dim=0)
-
-    # If it's validation, compute RMSE
-    if is_validation:
-        answers = torch.cat(answers, dim=0)
-        lossfn = nn.MSELoss()
-        rmse = torch.sqrt(lossfn(preds, answers)).item()
-        return rmse, preds, answers
-    else:
-        return preds
 
 # ======================= Main body function ========================== #
 def main(args, config):
@@ -176,7 +183,7 @@ def main(args, config):
         # === Dataset === #
         print("Creating dataset for fold ", fold + 1)
         name = args.name    
-        if name == 'HLM':
+        if name == 'MLM':
             dataset_train = SMILESDataset_SHIN_HLM('data/train.csv', mode='train', fold_num=fold)
             dataset_val = SMILESDataset_SHIN_HLM('data/train.csv', mode='val', fold_num=fold)
             dataset_test = SMILESDataset_SHIN_HLM('data/test.csv', mode='test')
@@ -184,7 +191,7 @@ def main(args, config):
             dataset_feature_val = FEATUREDataset('data/train_feature.csv', mode='val', fold_num=fold)
             dataset_feature_test = FEATUREDataset('data/test_feature.csv', mode='test')
 
-        elif name == 'MLM':
+        elif name == 'HLM':
             dataset_train = SMILESDataset_SHIN_MLM('data/train.csv', mode='train', fold_num=fold)
             dataset_val = SMILESDataset_SHIN_MLM('data/train.csv', mode='val', fold_num=fold)
             dataset_test = SMILESDataset_SHIN_MLM('data/test.csv', mode='test')
@@ -223,7 +230,7 @@ def main(args, config):
             train(model, train_loader, feature_train_loader, optimizer, tokenizer, epoch,  device)
 
             # ================== Validation datasets loaded ================== #
-            val_rmse, _, _ = evaluate(model, val_loader, feature_val_loader, tokenizer, device, is_validation=True)
+            val_rmse = evaluate(model, val_loader, feature_val_loader, tokenizer, device, is_validation=True)
             print(f'VALID MSE for fold {fold + 1}, epoch {epoch}: %.4f' % val_rmse)
             if val_rmse < best_valid:
                 best_valid = val_rmse
